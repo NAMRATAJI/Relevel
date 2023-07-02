b517c068f06a2dd3f2723136dfdeb2323a113d46
NAMRATA <namratajhait@gmail.com>
2023-07-02 15:16:32 +0530

    second commit

---
+++ pow.js
@@ -0,0 +1,12 @@
+function getPower(num, pow){
+    let i=1;
+    let result=1;
+    while(i<=pow){
+        result=result*num;
+        i++;
+    }
+    console.log(result);
+}
+getPower(10, 2); //100
+getPower(2, 5);
+getPower(4, 3);
